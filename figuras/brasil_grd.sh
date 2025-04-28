a32c9ccf00eb219e108d267d75cc6a0b3292609d
gustavo <gustavogosling2310@gmail.com>
2025-04-28 12:41:20 -0300

    Adiciona diretorio onde ficarão scripts que fazem as figuras para o .tex
    Separei o brasil_der.sh em brasil_der.sh e brasil_grd.sh, o último gerando a figura apenas

---
+++ figuras/brasil_grd.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#nome do arquivo
+name=`expr $1.ps`
+
+gmt psbasemap -R-76/-32/-36/8 -Bxya10 -BWNsE -JM12 -Xc -Y8 -K -P > name
+#~ gmt makecpt -T-1/1/0.1 -Cpolar > brasil_der.cpt
+gmt makecpt -T-1/1/0.1 -Cred2green > brasil_der.cpt
+gmt grdview  figrd.grd -R -J -O -K -Qi -Cbrasil_der.cpt >> name
+gmt pscoast -R -J -O -K -W0.5p -Dl -N1 -A10000+l >> name
+#~ cat brasil_1x1.xy | gmt psxy -R -J -O -K -W2p,red >> plot.ps
+cat brasil_05x05.xy | gmt psxy -R -J -O -K -W1p,green >> name
+gmt psxy -R -J -O -T >> name
+
+# -47/-43/-24/-21.5 vale do paraiba
+# -60/-52/-25/-14 pantanal
+# -42/-38/-8.2/-6.7 araripe
+
+#~ gmt grdpaste figrd.grd ligrd.grd -Gfigrd1.grd
+
+[ -f name ] && gv name
+
+gmt psconvert -P -A -Tf name
+gmt psconvert -P -A -TG name
+
+rm -f name brasil_der.cpt gmt.history
